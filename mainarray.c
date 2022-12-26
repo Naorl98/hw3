5247d775e4212ccb0083a7f1f213f92a85c2802a
Naorl98 <118805710+Naorl98@users.noreply.github.com>
2022-12-26 17:37:11 +0200

    Update mainarray.c

--- mainarray.c
+++ mainarray.c
@@ -12,7 +12,9 @@ int main(){
     insertion_sort(arr, SIZE);
     for (int i = 0; i < SIZE; i++)
     {
-        printf("%d,",*(arr+i));
+        if(i == (SIZE-1)) printf("%d",*(arr+i)); 
+        else printf("%d,",*(arr+i));
+        
     }
     return 0;
 }
