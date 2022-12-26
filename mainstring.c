47f4d3acd747965d4430c71da2726c41dbc82021
Naorl98 <118805710+Naorl98@users.noreply.github.com>
2022-12-26 06:36:42 +0200

    Update mainstring.c

--- mainstring.c
+++ mainstring.c
@@ -4,7 +4,7 @@
 #include "hw3String.h"
 #define SIZELINE 256
 #define SIZEWORD 30
-int newchar;
+int newchar = ' ';
 int getLine(char line[SIZELINE]){
     bzero(line,256);
     for(int i=0; i<SIZELINE; i++){ 
@@ -27,9 +27,9 @@ int getWord(char word[SIZEWORD] ){
 
 }
 int main(){
-    char line[SIZELINE];
-    char word[SIZEWORD];
-    char wordcheck[SIZEWORD];
+    char line[SIZELINE] = {0};
+    char word[SIZEWORD] = {0};;
+    char wordcheck[SIZEWORD] = {0};;
     if(getWord(word) ==0) exit(0);
     newchar = fgetc(stdin);
     if(newchar == 'a'){
