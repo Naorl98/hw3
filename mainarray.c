8e1a3be3bd1285e7e04e8fbf6c9ec57fc570b6e3
Naorl98 <118805710+Naorl98@users.noreply.github.com>
2022-12-26 06:27:52 +0200

    Update mainarray.c

--- mainarray.c
+++ mainarray.c
@@ -7,21 +7,13 @@ int main(){
     int arr[SIZE] = {0};
     for (int i = 0; i < SIZE; i++)
     {
-        printf("enter num:\n");
         scanf("%d",(arr+i));
     }
-    for (int i = 0; i < SIZE; i++)
-    {
-        printf("%d,",*(arr+i));
-    }
-    printf("\n");
     insertion_sort(arr, SIZE);
-    printf("The sorted array: \n");
     for (int i = 0; i < SIZE; i++)
     {
         printf("%d,",*(arr+i));
     }
-    printf("\n");
     return 0;
 }
 
